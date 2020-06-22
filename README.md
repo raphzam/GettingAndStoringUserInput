e98cab93d8b685dfb0bd9a32acda86bd80d44ab6
raphzam <raphaeljasonzamora@gmail.com>
2020-06-22 16:58:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Getting and Storing User Input
+
+Prompt user for their name and address fields and print the result.
---
+++ src/GettingAndStoringUserInput.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GettingAndStoringUserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        DECLARING VARIABLES
+
+        String firstName;
+        String lastName;
+        int houseNumber;
+        String streetName;
+        String city;
+
+//        GETTING INPUT
+        System.out.println("First name:");
+        firstName = input.nextLine();
+
+        System.out.println("Last name:");
+        lastName = input.nextLine();
+
+        System.out.println("House number:");
+        houseNumber = input.nextInt();
+        input.nextLine();       //buffer
+
+
+        System.out.println("Street name:");
+        streetName = input.nextLine();
+
+        System.out.println("City:");
+        city = input.nextLine();
+
+        System.out.println(firstName + " " + lastName + " " + houseNumber + " " + streetName + " " + city);
+
+
+    }
+}
